44622548230740a7f6541019088465f1c7400048
kellyn <kellynwong@hotmail.com>
2024-11-17 01:30:35 +0800

    Change metadata - icon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,42 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Web3Auth NextJS Quick Start",
-  description: "Web3Auth NextJS Quick Start",
+  title: "banana babies",
+  description: "A Tropical Adventure",
+  icons: {
+    icon: '/bblogo.png',
+    shortcut: '/bblogo.png',
+    apple: '/bblogo.png',
+    // For better compatibility with different devices
+    other: [
+      {
+        rel: 'apple-touch-icon',
+        url: '/bblogo.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/bblogo.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/bblogo.png',
+      },
+    ],
+  },
 };
 
 // eslint-disable-next-line no-undef
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/bblogo.png" />
+        <link rel="apple-touch-icon" href="/bblogo.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
