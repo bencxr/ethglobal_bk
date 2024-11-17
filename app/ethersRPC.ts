bfa5459603710c9e00d110d20c0982ae92b6e325
Benedict Chan <bencxr@fragnetics.com>
2024-11-17 07:03:33 +0700

    Revert "remove this due to errors in vercel"
    This reverts commit b589428496f71fce6ba5a1c2c5b8ee6187fc5bf8.

--- app/ethersRPC.ts
+++ app/ethersRPC.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { IProvider } from "@web3auth/base";
 import { ethers } from "ethers";
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable no-console */
 "use client";
 
 import { useCallback, useEffect, useState } from "react";
